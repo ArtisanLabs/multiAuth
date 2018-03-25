2c782652b2aa1aae605c05cb5dd37d526aa4f76d
ArtisanLabs <artisan254@gmail.com>
2018-03-25 18:09:52 +0300

    Created the view for the admin dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,7 +1,23 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: artisan
- * Date: 3/25/18
- * Time: 5:49 PM
- */
+@extends('layouts.app')
+
+@section('content')
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-md-8">
+				<div class="card">
+					<div class="card-header">Admin Dashboard</div>
+
+					<div class="card-body">
+						@if (session('status'))
+							<div class="alert alert-success">
+								{{ session('status') }}
+							</div>
+						@endif
+
+						You are logged in as an admin!
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
