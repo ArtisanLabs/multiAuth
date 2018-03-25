dccfe27577ff9705ad836cedab944c83b7afb80d
ArtisanLabs <artisan254@gmail.com>
2018-03-25 18:10:19 +0300

    Added the routes for admin authentication and dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,30 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+/**
+ * All the admin routes go here
+ * @prefix admin
+*/
+Route::prefix('admin')->group(function (){
+	Route::get('/login', [
+		'uses' => 'PageController@getAdminLogin',
+		'as' => 'admin.login'
+	]);
+
+	Route::post('/login', [
+		'uses' => 'PageController@authAdmin',
+		'as' => 'admin.login'
+	]);
+
+	// Route to admins dashboard
+	Route::get('/home', [
+		'uses' => 'AdminController@index',
+		'as' => 'admin.home'
+	]);
+});
+
